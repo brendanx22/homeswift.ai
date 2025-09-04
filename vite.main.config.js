18fbae77b0d727d88a657f61762df38ad4d0e685
Nwanze Brendan <nwanzebrendan@gmail.com>
2025-09-04 10:11:26 +0100

    commit

--- vite.main.config.js
+++ vite.main.config.js
@@ -3,10 +3,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'landing',
   plugins: [react()],
   build: {
     emptyOutDir: true,
-    outDir: '../dist',   // ⬅️ change from ../main to ../dist
+    rollupOptions: {
+      input: {
+        index: 'landing.html',
+      },
+    },
   },
 })
