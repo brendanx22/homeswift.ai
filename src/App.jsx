e1646f850ff57871ea23a9fa0222b8eb1fbda67a
Nwanze Brendan <nwanzebrendan@gmail.com>
2025-09-06 05:09:59 +0100

    Vite error

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/hero/LoginPage';
 import SignupPage from './components/hero/SignupPage';
 import MainLanding from './components/main/MainLanding';
-import PropertyDetails from './PropertyDetails';
+import PropertyDetails from './components/main/PropertyDetails';
 import GoogleAuthCallback from './components/auth/GoogleAuthCallback';
 import Listings from './components/main/Listings';
 import './index.css';
