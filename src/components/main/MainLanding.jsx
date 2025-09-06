ce9f81a5b5426223cd84e936862fdea9d6929ba7
Uti Favour <utifavour15@gmail.com>
2025-09-06 12:54:44 +0100

    Update

--- src/components/main/MainLanding.jsx
+++ src/components/main/MainLanding.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+
+
 import { useNavigate } from "react-router-dom";
 import {
   Menu,
